bed4fa5085b5aeda08c85a807fc6c9071d3cc246
sustained <sustained.dissonance@gmail.com>
2019-02-24 02:24:32 +0000

    Add App.vue and main.js.

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+html,
+body,
+div#app {
+  height: 100%;
+  width: max-content;
+  font-family: sans-serif;
+  overflow-x: hidden;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
