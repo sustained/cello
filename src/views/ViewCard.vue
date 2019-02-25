fd45a18ddec10e729c4a4dfaf2979a24636c2666
sustained <sustained.dissonance@gmail.com>
2019-02-25 23:18:43 +0000

    Use fixed not absolute.

--- src/views/ViewCard.vue
+++ src/views/ViewCard.vue
@@ -19,7 +19,7 @@ export default {
 div.background {
   background-color: rgba(0, 0, 0, 0.5);
 
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
@@ -27,7 +27,7 @@ div.background {
 }
 
 div.wrapper {
-  position: absolute;
+  position: fixed;
   top: 15vh;
   bottom: 15vh;
   left: 15vw;
