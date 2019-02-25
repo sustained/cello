c864aab96d0811d04ede138a9744cce2a1d0f226
sustained <sustained.dissonance@gmail.com>
2019-02-25 22:17:52 +0000

    Add view board component.

--- src/views/ViewBoard.vue
+++ src/views/ViewBoard.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="board">
-    <div class="grid" :style="style">
-      <list v-for="list in board.lists" :key="list.id" :selected="isSelected(list.id)" ref="lists">
-        <card v-for="card in cardsForList(list.id)" :key="card.id" :card="card"></card>
+    <router-view></router-view>
+
+    <div class="grid" v-for="(group, groupIndex) in groups" :key="groupIndex">
+      <list
+        v-for="(list, listIndex) in group.lists"
+        :key="list.id"
+        :selected="isSelected(listIndex, groupIndex)"
+        :ref="`lists-${groupIndex}`"
+      >
+        <!-- TODO: ^ Get rid of refs. -->
+        <!-- NOTE: ^ group.lists was before "the changes" - cardsForList (vuex) -->
+        <card v-for="card in list.cards" :key="card.id" :card="card"></card>
       </list>
     </div>
   </div>
@@ -22,18 +31,199 @@ export default {
 
   data() {
     return {
-      currentList: 0
+      currentGroup: 0,
+      currentList: 0,
+
+      groups: [
+        {
+          lists: [
+            { id: 1, title: "foo", cards: [{ id: 1, title: "gsge" }] },
+            { id: 2, title: "bar", cards: [{ id: 2, title: "ajge" }] },
+            { id: 3, title: "blah", cards: [{ id: 3, title: "oejg" }] },
+            { id: 4, title: "weee", cards: [{ id: 4, title: "oejg" }] },
+            { id: 5, title: "doode", cards: [{ id: 5, title: "oejg" }] },
+            { id: 6, title: "dumda", cards: [{ id: 6, title: "oejg" }] },
+            { id: 7, title: "diddely", cards: [{ id: 7, title: "oejg" }] },
+            { id: 8, title: "bloooo", cards: [{ id: 8, title: "oejg" }] }
+          ]
+        },
+        {
+          lists: [
+            { id: 11, title: "bazfoo", cards: [{ id: 11, title: "meut" }] },
+            { id: 12, title: "foobar", cards: [{ id: 12, title: "wjgi" }] },
+            { id: 13, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 14, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 15, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 16, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 17, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 18, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 19, title: "bazbar", cards: [{ id: 13, title: "pegj" }] },
+            { id: 20, title: "bazbar", cards: [{ id: 13, title: "pegj" }] }
+          ]
+        }
+      ]
     };
   },
 
-  created() {},
+  beforeDestroy() {
+    console.info("cleaning up event listeners");
+    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener("mousewheel", this.handleMouseWheel);
+  },
 
-  mounted() {
+  async mounted() {
     window.scrollTo(0, 0);
 
-    document.addEventListener("keydown", event => {
-      console.log(event, event.keyCode);
+    /*
+    // NOTE: Quickly generate lots of data to stress-test.
+    let currListId = 0;
+    let currCardId = 0;
 
+    for (let i = 0; i < 30; i++) {
+      const group = { lists: [] };
+
+      for (let j = 0, jm = Math.floor(Math.random() * 30); j < jm; j++) {
+        const list = { id: currListId++, title: Math.random().toString(36).slice(2), cards: [] };
+
+        for (let k = 0, km = Math.floor(Math.random() * 30); k < km; k++) {
+          const card = { id: currCardId++, title: Math.random().toString(36).slice(2) };
+
+          list.cards.push(card);
+        }
+
+        group.lists.push(list);
+      }
+
+      Vue.set(this.groups, i, group);
+    }
+    */
+
+    console.info("registering event listeners");
+
+    document.addEventListener("keydown", this.handleKeyDown);
+
+    document.addEventListener("mousewheel", this.handleMouseWheel, {
+      passive: true
+    });
+
+    if (this.$route.params.id)
+      this.$store.dispatch("loadBoard", this.$route.params.id);
+  },
+
+  computed: {
+    ...mapState(["board"]),
+    ...mapGetters(["cardsForList"]),
+
+    // overflows() {
+    //   return document.body.scrollWidth > document.body.clientWidth;
+    // },
+
+    style() {
+      return {
+        // "grid-template-columns": `repeat(${this.board.lists.length}, 200px)`
+      };
+    }
+  },
+
+  methods: {
+    isSelected(listId, groupIndex) {
+      // NOTE: DB indexes start at 1 but we switched to using local state for the new navigation prototype thing.
+      return listId === this.currentList && groupIndex === this.currentGroup;
+    },
+
+    /*
+      TODO: Move all navigation-related stuff to Vuex.
+
+      We can store a mapping of groupId => listCount (needed for when we navigate up/down).
+    */
+
+    scrollLeft() {
+      if (--this.currentList < 0)
+        this.currentList = this.$refs["lists-" + this.currentGroup].length - 1;
+
+      this.scrollToCurrentList();
+    },
+
+    scrollRight() {
+      if (
+        ++this.currentList >
+        this.$refs["lists-" + this.currentGroup].length - 1
+      )
+        this.currentList = 0;
+
+      this.scrollToCurrentList();
+    },
+
+    scrollDown() {
+      let didWrap = false;
+
+      // Bottom wraps back around to top.
+      if (++this.currentGroup > this.groups.length - 1) {
+        didWrap = true;
+        this.currentGroup = 0;
+      }
+
+      // Select final list if we were scrolled further right in the previous group.
+      if (
+        this.currentList >
+        this.$refs["lists-" + this.currentGroup].length - 1
+      )
+        this.currentList = this.$refs["lists-" + this.currentGroup].length - 1;
+
+      this.scrollToCurrentList(didWrap);
+    },
+
+    scrollUp() {
+      let didWrap = false;
+
+      // Top wraps back around to top.
+      if (--this.currentGroup < 0) {
+        didWrap = true;
+        this.currentGroup = this.groups.length - 1;
+      }
+
+      // Select final list if we were scrolled further right in the previous group.
+      if (
+        this.currentList >
+        this.$refs["lists-" + this.currentGroup].length - 1
+      )
+        this.currentList = this.$refs["lists-" + this.currentGroup].length - 1;
+
+      this.scrollToCurrentList(didWrap);
+    },
+
+    /* IDEA: We could intelligently handle the disableSmoothBehaviour thing.
+      
+      The scrolling with the smooth behaviour enabled with large numbers of groups 
+      is extremely jarring (and may well be (one of) the cause(s) of the frame rate 
+      drops from the performance test (mostly 60 FPS but the odd dip to ~10).
+
+      We can probably make it so that if there are too many groups, then it will just 
+      intelligently set behaviour to "auto".
+    */
+    scrollToCurrentList(disableSmoothBehaviour) {
+      const lists = this.$refs["lists-" + this.currentGroup];
+
+      if (!lists) return console.warn("no refs for group " + this.currentGroup);
+
+      const component = lists[this.currentList];
+
+      if (!component || !component.$el)
+        return console.warn(
+          "no component/el for group " + this.currentGroup,
+          component,
+          component.$el
+        );
+
+      const element = component.$el;
+
+      element.scrollIntoView({
+        behavior: disableSmoothBehaviour ? "auto" : "smooth", // TODO: Let user choose auto/smooth.
+        inline: "center"
+      });
+    },
+
+    handleKeyDown(event) {
       switch (event.keyCode) {
         case 65:
           this.scrollLeft();
@@ -42,88 +232,38 @@ export default {
         case 68:
           this.scrollRight();
           break;
-      }
-    });
 
-    document.addEventListener(
-      "mousewheel",
-      throttle(event => {
+        case 87:
+          this.scrollUp();
+          break;
+
+        case 83:
+          this.scrollDown();
+          break;
+      }
+    },
+
+    handleMouseWheel(event) {
+      /*
+      return throttle(() => {
+        event.preventDefault();
+
         let current = this.$refs.lists[this.currentList];
 
         if (!current)
-          return console.debug(
-            "couldn't find list???",
-            this.$refs.lists.length,
-            this.currentList
-          );
-
-        // console.log(window.scrollX, current.$el.offsetLeft);
+          return console.warn("couldn't find list " + this.currentList);
 
         if (event.deltaY < 0) this.currentList--;
         else if (event.deltaY > 0) this.currentList++;
 
         if (this.currentList < 0)
           this.currentList = this.$refs.lists.length - 1;
-        if (this.currentList > this.$refs.lists.length - 1)
+        else if (this.currentList > this.$refs.lists.length - 1)
           this.currentList = 0;
 
         this.scrollToCurrentList();
-        // window.scrollTo({
-        //   top: 0,
-        //   left: Math.max(
-        //     0,
-        //     this.$refs.lists[this.currentList].$el.offsetLeft - 16
-        //   )
-        // });
-      }, 50),
-      { passive: true }
-    );
-
-    if (this.$route.params.id) {
-      this.$store.dispatch("loadBoard", this.$route.params.id);
-    }
-  },
-
-  computed: {
-    ...mapState(["board"]),
-    ...mapGetters(["cardsForList"]),
-
-    overflows() {
-      return document.body.scrollWidth > document.body.clientWidth;
-    },
-
-    style() {
-      return {
-        "grid-template-columns": `repeat(${this.board.lists.length}, 200px)`
-      };
-    }
-  },
-
-  methods: {
-    isSelected(listId) {
-      // DB indexes start at 1.
-      return listId - 1 === this.currentList;
-    },
-
-    scrollLeft() {
-      if (--this.currentList < 0)
-        this.currentList = this.$refs.lists.length - 1;
-      this.scrollToCurrentList();
-    },
-
-    scrollRight() {
-      if (++this.currentList > this.$refs.lists.length - 1)
-        this.currentList = 0;
-      this.scrollToCurrentList();
-    },
-
-    scrollToCurrentList() {
-      if (!this.$refs.lists) return console.warn("no lists refs???");
-      const element = this.$refs.lists[this.currentList].$el;
-      element.scrollIntoView({
-        behavior: "smooth", // TODO: Let user choose.
-        inline: "center"
-      });
+      }, 50);
+      */
     }
   }
 };
@@ -132,13 +272,17 @@ export default {
 <style>
 div#board,
 div.grid {
-  height: 100%;
-  width: 100%;
+  height: 100vh;
 }
 
 div.grid {
+  display: flex;
+  flex-direction: row;
+  padding: 0.5em;
+  /*
   display: grid;
   grid-gap: 1em;
   padding: 1em;
+  */
 }
 </style>
