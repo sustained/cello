2574144683d1e846049a014a67b1af2f073beb6f
sustained <sustained.dissonance@gmail.com>
2019-02-25 22:18:09 +0000

    Add view card component.

---
+++ src/views/ViewCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="background">
+    <div class="wrapper">
+      <p>{{ card }}</p>
+      <p>
+        <a @click="$router.go(-1)">close card</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card"]
+};
+</script>
+
+<style>
+div.background {
+  background-color: rgba(0, 0, 0, 0.5);
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+div.wrapper {
+  position: absolute;
+  top: 15vh;
+  bottom: 15vh;
+  left: 15vw;
+  right: 15vw;
+  background-color: rgba(0, 0, 255, 0.25);
+}
+
+p {
+  margin: 1em;
+}
+</style>
