7ea6c5dce1217dbaf4bc9df8374203987681bf78
sustained <sustained.dissonance@gmail.com>
2019-02-25 22:11:58 +0000

    Add card component.

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="card">
-    <p>
-      <strong>Card #{{ card.id }}</strong>
-    </p>
+    <!--NOTE: Couldn't get this working by passing an object with route: 'card', 
+    investigate more + note that append here can cause issues in some cases.-->
+    <router-link :to="`card/${card.id}`" append>
+      <p>{{ card.title }}</p>
+    </router-link>
   </div>
 </template>
 
@@ -17,5 +19,10 @@ div.card {
   background-color: rgb(0, 255, 0, 0.3);
   padding: 10px;
   margin: 10px;
+}
+
+div.card pre {
+  margin-top: 1em;
+  font-size: 0.5em;
 }
 </style>
