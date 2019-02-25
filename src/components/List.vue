a8dde9836bbea77918eda4f9f361ee38742049d4
sustained <sustained.dissonance@gmail.com>
2019-02-25 22:13:38 +0000

    Add list component.

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,33 +1,58 @@
 <template>
   <div ref="el" :class="['list', {selected}]">
-    <p>List!</p>
+    <div class="list-wrapper">
+      <p>List!</p>
 
-    <slot>
-      <p style="margin: 10px;">No cards!</p>
-    </slot>
+      <slot>
+        <p style="margin: 20px 10px;">No cards!</p>
+      </slot>
+    </div>
+
+    <div class="list-add-card">
+      <router-link to="card/add" append>Add Card</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-
 export default {
   props: ["selected"]
 };
 </script>
 
 <style scoped>
+div.list-wrapper {
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  flex-grow: 1;
+}
+
+div.list-add-card {
+  padding: 0.5em;
+}
+
 div.list {
+  width: 222px;
+
+  display: flex;
+  flex-direction: column;
+
   background-color: rgba(255, 0, 0, 0.3);
 }
 
 div.list {
-  height: 100%;
   border: 2px solid transparent;
-  grid-row: 1 / -1;
+  margin-right: 0.5em;
 }
-
+div.list:last-child {
+  margin-right: 0;
+}
 div.list.selected {
   border: 2px solid black;
+}
+
+p {
+  margin: 10px;
 }
 </style>
