4e53e2bf75ea78036d04846360d1b83aeca204d7
sustained <sustained.dissonance@gmail.com>
2019-02-26 08:59:51 +0000

    Add board introduction view.

---
+++ src/views/BoardIntroduction.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-show="boardIsEmpty">
+    <p>Board is empty!</p>
+    <p>Show guide/introduction?</p>
+    <p>Offer to create a board from a template?</p>
+    <p>Etc.</p>
+
+    <ul>
+      <li>
+        <router-link to="create/group">Create Group</router-link>
+      </li>
+      <li>
+        <router-link to="create/list">Create List</router-link>
+      </li>
+      <li>
+        <router-link to="create/card" disabled>Create Card</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: mapGetters(["boardIsEmpty"])
+};
+</script>
+
+<style scoped>
+</style>
